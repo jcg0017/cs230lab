dfba43a6c386cafdf58da8a85f47306b9e6f38ad
jcg0017 <jcg0017@mix.wvu.edu>
2021-02-06 04:21:38 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,57 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+    
+    <title>TV Review</title>
+    
+    <div class="about">
+        <h1 style="color: lightslategray">About Us</h1>
+        <p>After completing the gym challenges in the Kanto region, Pikachu decided to take a break from adventuring
+            and binge tv shows. First, he called his friend Eevee who recommended her trainer, Ariana Grande, for her
+            superior software developement skills. Meanwhile, in Ooo, Princess Bubblegum (big fan of Ari's) heard of 
+            Pikachu's plan and ordered Peppermint Butler to help them and report back their findings through a website,
+            for science purposes. Lord Voldy just wanted something to do after failing to kill Harry Potter. Welcome!
+        </p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>Project Manager</h2>
+                    <p class="c1">Pikachu</p>
+                    <p>Email: pokemonp@230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.png">
+                <div class="container">
+                    <h2>Developer</h2>
+                    <p class="c2">Ariana Grande</p>
+                    <p style="color: hotpink">Email: ari@230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.png">
+                <div class="container">
+                    <h2>Information Architect</h2>
+                    <p class="c3">Peppermint Butler</p>
+                    <p>Email: flesheater@230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>Analytics Specialist</h2>
+                    <p class="c4">Lord Voldy</p>
+                    <p>Email: parryhotter@230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
